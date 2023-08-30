3424501f1dcafba02524d62582c5ec7bf7728bc0
perrosx <carlos.ulloa2020@alu.uct.cl>
2023-08-30 16:47:31 -0400

    Union del proyecto
    uni lo de la rama msierra a lo que tenia en la rama culloa, es probable que falten paquetes o similares de la rama tsheel y la rama avalenzuela.

---
+++ zafiro/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import Navegador from './src/componets/navegation.js';
+
+export default function App() {
+  return <Navegador/>
+}
+
+
