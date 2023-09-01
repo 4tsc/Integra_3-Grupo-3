09887151809b52d2e0b07f5ec52bc7ae2b5b874b
4tsc <tomascheel@outlook.com>
2023-09-01 17:51:25 -0400

    estructura oficial

--- zafiro/database/bdprevisorioa.sql
+++ zafiro/database/bdprevisorioa.sql
@@ -1,16 +1,24 @@
 -- Crear la tabla "usuario"
 CREATE TABLE usuario (
     id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    rut VARCHAR(12) NOT NULL,
     correo VARCHAR(100) NOT NULL,
-    nombre VARCHAR(50) NOT NULL
+    facultad VARCHAR(100) NOT NULL,
+    t_planta BOOLEAN NOT NULL
 );
 
 -- Crear la tabla "asesor"
 CREATE TABLE asesor (
     id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    rut VARCHAR(12) NOT NULL,
     correo VARCHAR(100) NOT NULL,
-    nombre VARCHAR(50) NOT NULL
+    area VARCHAR(100) NOT NULL
 );
+
 
 -- Crear la tabla "horas"
 CREATE TABLE horas (
