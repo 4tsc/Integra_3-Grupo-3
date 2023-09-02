0845a6de835cff585894d5c6cf5e6c7eb77d2f9f
Andrews <apetey16@gmail.com>
2023-09-02 19:04:33 -0400

    Copia de archivos develop
    Archivos necesarios para no cagarla

---
+++ zafiro/database/bdprevisorioa.sql
@@ -0,0 +1,33 @@
+-- Crear la tabla "usuario"
+CREATE TABLE usuario (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    rut VARCHAR(12) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    facultad VARCHAR(100) NOT NULL,
+    t_planta BOOLEAN NOT NULL
+);
+
+-- Crear la tabla "asesor"
+CREATE TABLE asesor (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    rut VARCHAR(12) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    area VARCHAR(100) NOT NULL
+);
+
+
+-- Crear la tabla "horas"
+CREATE TABLE horas (
+    id INT PRIMARY KEY,
+    descripcion VARCHAR(100) NOT NULL,
+    hora TIME NOT NULL,
+    fecha DATE NOT NULL,
+    id_usuario INT,
+    id_asesor INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    FOREIGN KEY (id_asesor) REFERENCES asesor(id)
+);
