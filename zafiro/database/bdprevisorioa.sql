24cc5fbe8da7548462b96fba8118221a80062619
4tsc <tomascheel@outlook.com>
2023-09-03 18:59:36 -0300

    modificaciones bd

--- zafiro/database/bdprevisorioa.sql
+++ zafiro/database/bdprevisorioa.sql
@@ -1,4 +1,3 @@
--- Crear la tabla "usuario"
 CREATE TABLE usuario (
     id INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
@@ -6,10 +5,9 @@ CREATE TABLE usuario (
     rut VARCHAR(12) NOT NULL,
     correo VARCHAR(100) NOT NULL,
     facultad VARCHAR(100) NOT NULL,
-    t_planta BOOLEAN NOT NULL
+    t_planta VARCHAR(100) NOT NULL
 );
 
--- Crear la tabla "asesor"
 CREATE TABLE asesor (
     id INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
@@ -19,8 +17,6 @@ CREATE TABLE asesor (
     area VARCHAR(100) NOT NULL
 );
 
-
--- Crear la tabla "horas"
 CREATE TABLE horas (
     id INT PRIMARY KEY,
     descripcion VARCHAR(100) NOT NULL,
@@ -31,3 +27,15 @@ CREATE TABLE horas (
     FOREIGN KEY (id_usuario) REFERENCES usuario(id),
     FOREIGN KEY (id_asesor) REFERENCES asesor(id)
 );
+
+INSERT INTO usuario (id, nombre, apellido, rut, correo, facultad, t_planta)
+VALUES
+    (1, 'Juan', 'Pérez', '12345678-9', 'juan.perez@example.com', 'Facultad de Ciencias', 'partime'),
+    (2, 'María', 'Gómez', '98765432-1', 'maria.gomez@example.com', 'Facultad de Letras', 'permanente'),
+    (3, 'Pedro', 'Sánchez', '34567890-1', 'pedro.sanchez@example.com', 'Facultad de Artes', 'adjunta');
+
+INSERT INTO asesor (id, nombre, apellido, rut, correo, area)
+VALUES
+    (1, 'Carlos', 'López', '45678912-3', 'carlos.lopez@example.com', 'virtualización'),
+    (2, 'Ana', 'Martínez', '78912345-6', 'ana.martinez@example.com', 'comunidades'),
+    (3, 'Laura', 'García', '23456789-0', 'laura.garcia@example.com', 'virtualización');
