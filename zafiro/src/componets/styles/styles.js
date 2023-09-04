2b13914e19db7ea805c2cae9a8888cac372109c3
Maximiiano Sierra <msierra2021@alu.uct.cl>
2023-09-03 23:26:40 -0300

    implementación bd en la app y prueba para poder registrar horas
    se creo la implementación de la base de datos en la aplicación, también se hizo una prueba para que se registren nuevas horas en esta, aunque esta no funciona

---
+++ zafiro/src/componets/styles/styles.js
@@ -0,0 +1,122 @@
+import { StyleSheet, StatusBar } from "react-native";
+
+const styles_log = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0085FF',
+        marginTop: StatusBar.currentHeight
+      },
+      container2: {
+        flex: 1,
+        alignItems: 'center',
+        top: '10%',
+      },
+      container3: {
+        alignItems: 'center',
+        flex: 1,
+        top: "5%",
+      },
+      container4:{
+        alignItems: 'center',
+        flex: 1,
+        top: '-3%',
+      },
+      container5:{
+        alignItems: 'center',
+        flex: 1,
+        bottom: '10%'
+      },
+      texto1:{
+    
+      },
+      texto2:{
+    
+      },
+      texto3:{
+        alignItems: 'center',
+        top: '-27%'
+      },
+      testo:{
+        color: '#FFF',
+        fontSize: 12
+      },
+      input: {
+        width: 264,
+        height: 49,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 10,
+        padding: 5,
+        backgroundColor: '#D9D9D9',
+        borderRadius: 70,
+        textAlign: 'center',
+      },
+      btn: {
+        backgroundColor:'#FAB700',
+        borderRadius: 100,
+        height:48,
+        width:144,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      btnText: {
+        color: 'black',
+        fontWeight: 'bold',
+      },
+      image: {
+        width: 100,
+        height: 93,
+        resizeMode: 'contain',
+      },
+})
+const styles_Principal = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  pickerBox: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 20,
+  },
+  selectedDate: {
+    marginTop: 20,
+    fontSize: 16,
+  },
+})
+
+const styles_Agendar = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pickerBox: {
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    marginBottom: 20,
+  },
+  selectedDateTime: {
+    marginTop: 10,
+    fontSize: 16,
+  },
+  advisorPicker: {
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    marginBottom: 20,
+  },
+  advisorButtons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+});
+export {styles_log, styles_Principal, styles_Agendar};
