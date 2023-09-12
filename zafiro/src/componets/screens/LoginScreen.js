850ed56d5f61941a27f5ba03687054253a62f336
4tsc <tomascheel@outlook.com>
2023-09-12 00:59:52 -0300

    login funcional v1

--- zafiro/src/componets/screens/LoginScreen.js
+++ zafiro/src/componets/screens/LoginScreen.js
@@ -1,31 +1,51 @@
 import React, { useState } from 'react';
-import { View, Text, Pressable,TextInput, Image, StatusBar} from 'react-native';
-import {styles_log} from '../styles/styles.js';
+import { View, Text, Pressable, TextInput, Image, StatusBar } from 'react-native';
+import { styles_log } from '../styles/styles.js';
 import { useNavigation } from '@react-navigation/native';
 
-
 const Log_in = () => {
-    const [text, setText] = useState('');
-    const [text2, setText2] = useState('');
-    const navigation = useNavigation();
-    const handlePress = () => {
-      navigation.navigate('PrincipalScreen');
+  const [text, setText] = useState('');
+  const [text2, setText2] = useState('');
+  const navigation = useNavigation();
+
+  const handlePress = async () => {
+    // Crear un objeto con los datos a enviar
+    const data = {
+      correo: text,
+      contraseña: text2,
     };
 
+    try {
+      const response = await fetch('http://localhost:8080/auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', // Indicar que se está enviando JSON
+        },
+        body: JSON.stringify(data), // Convertir el objeto a JSON
+      });
+
+      if (response.status === 200) {
+        // Autenticación exitosa, redireccionar u otras acciones
+        navigation.navigate('PrincipalScreen');
+      } else {
+        // Manejar la respuesta de autenticación fallida aquí
+        // Puedes mostrar un mensaje de error o realizar otras acciones
+      }
+    } catch (error) {
+      // Manejar errores de red u otros errores aquí
+    }
+  };
+
   return (
-    //probando 
     <View style={styles_log.container}>
       <StatusBar barStyle="content" backgroundColor="#5D5D5D" />
       <View style={styles_log.container2}>
-        {/* Logo UCT debemos mejorar esto */}
         <Image
           source={require('../images/Logo_UCT.png')}
           style={styles_log.image}
         />
       </View>
 
-
-        {/* Input de  Correo Institucional que debe pasar por BD*/}
       <View style={styles_log.container3}>
         <View style={styles_log.texto1}>
           <Text style={styles_log.testo}>Correo Institucional</Text>
@@ -37,9 +57,7 @@ const Log_in = () => {
           value={text}
         />
       </View>
-        
 
-        {/* Input de  Contraseña que debe pasar por BD*/}
       <View style={styles_log.container4}>
         <View style={styles_log.texto2}>
           <Text style={styles_log.testo}>Contraseña</Text>
@@ -52,20 +70,20 @@ const Log_in = () => {
           value={text2}
         />
       </View>
-        {/* Boton de iniciar sesion */}
+
       <View style={styles_log.container5}>
         <Pressable style={styles_log.btn} onPress={handlePress}>
           <Text style={styles_log.btnText}>Iniciar Sesión</Text>
         </Pressable>
       </View>
+
       <View style={styles_log.texto3}>
-        {/* Al hacer click aqui se debe redireccionar al sitio de la u para cambiar Contraseña */}
         <Pressable>
           <Text style={styles_log.testo}>¿Olvidaste tu contraseña?</Text>
         </Pressable>
       </View>
-
     </View>
-  );}
+  );
+}
 
-  export default Log_in;
+export default Log_in;
