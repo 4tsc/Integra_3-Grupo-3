aaee3c1bc0ce937e41a40e702e6fae9d6fe522fd
Andrews <apetey16@gmail.com>
2023-10-09 23:02:22 -0300

    seting
    nada importante, sigo sin poder avanzar en la api, me obligo hacer un push para cambiar de rama

--- zafiro/src/componets/screens/settings.js
+++ zafiro/src/componets/screens/settings.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { styles_settings } from '../styles/styles.js';
+
 function ProfileScreen() {
   const [name, setName] = useState('John');
   const [lastName, setLastName] = useState('Doe');
@@ -9,6 +10,7 @@ function ProfileScreen() {
   const [rut, setRut] = useState('123456789');
   const [phoneNumber, setPhoneNumber] = useState('123456789');
   const [image, setImage] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     // Solicitar permisos para acceder a la galería de imágenes
@@ -35,43 +37,53 @@ function ProfileScreen() {
 
   return (
     <View style={styles_settings.container1}>
-      {image && <Image source={{ uri: image }} style={styles_settings.profileImage} />}
-      <TouchableOpacity onPress={pickImage}>
-        <Text>Editar Foto de Usuario</Text>
+      <TouchableOpacity onPress={pickImage} style={styles_settings.frame}>
+        {image ? (
+          <Image source={{ uri: image }} style={styles_settings.profileImage} />
+        ) : (
+          <Text>Editar Foto de Usuario</Text>
+        )}
       </TouchableOpacity>
-      <TextInput
-        style={styles_settings.input}
-        placeholder="Nombre"
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        style={styles_settings.input}
-        placeholder="Apellido"
-        value={lastName}
-        onChangeText={setLastName}
-      />
-      <TextInput
-        style={styles_settings.input}
-        placeholder="Correo Electrónico"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles_settings.input}
-        placeholder="Rut"
-        value={rut}
-        onChangeText={setRut}
-      />
-      <TextInput
-        style={styles_settings.input}
-        placeholder="Número Telefónico"
-        keyboardType="numeric"
-        maxLength={9}
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-      />
-      <Button title="Guardar Cambios" onPress={() => console.log('Guardar cambios')} />
+
+      {isEditing ? (
+        <>
+          <TextInput
+            style={styles_settings.input}
+            placeholder="Nombre"
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            style={styles_settings.input}
+            placeholder="Apellido"
+            value={lastName}
+            onChangeText={setLastName}
+          />
+          <TextInput
+            style={styles_settings.input}
+            placeholder="Correo Electrónico"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            style={styles_settings.input}
+            placeholder="Rut"
+            value={rut}
+            onChangeText={setRut}
+          />
+          <TextInput
+            style={styles_settings.input}
+            placeholder="Número Telefónico"
+            keyboardType="numeric"
+            maxLength={9}
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+          <Button title="Guardar Cambios" onPress={() => setIsEditing(false)} />
+        </>
+      ) : (
+        <Button title="Editar Cambios" onPress={() => setIsEditing(true)} />
+      )}
     </View>
   );
 }
